6db1fd90cbe3a224aee3bbb961803fcbd1bffbe7
ne0en <ne0en@EliteBook>
2012-06-05 22:06:59 +0200

    New layout for retweet,...

---
+++ src/si/fis/android/twittapp/RetweetActivity.java
@@ -0,0 +1,14 @@
+package si.fis.android.twittapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RetweetActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
--- src/si/fis/android/twittapp/TweetsActivity.java
+++ src/si/fis/android/twittapp/TweetsActivity.java
@@ -1,55 +1,85 @@
 package si.fis.android.twittapp;
 
 import twitter4j.ResponseList;
-import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class TweetsActivity extends ListActivity implements OnClickListener {
 	Button buttonPost;
 	ListView listV;
 	Twitter twitter;
-	ResponseList<Status> statuses;
-	
+
 	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	
-    	super.onCreate(savedInstanceState);
-        setContentView(R.layout.tweets);
-        
-        twitter = ((TwittApplication) getApplication()).getTwitter();
-        
-        buttonPost = (Button) findViewById(R.id.buttonPost);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tweets);
+
+		buttonPost = (Button) findViewById(R.id.buttonPost);
 		buttonPost.setOnClickListener(this);
-		
-		
-		listV = (ListView) findViewById(android.R.id.list);
-		
-		listV.setAdapter(new TweetAdapter(this, R.layout.tweet_row, getStatuses()));
-    }
-    
-    public ResponseList<Status> getStatuses() {
-		try {
-			statuses = twitter.getHomeTimeline();
-		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        return statuses;
-    }
+		new TwitterGetHomeline().execute();
+	}
+
 	@Override
 	public void onClick(View v) {
 		if (v.getId() == R.id.buttonPost) {
 			startActivity(new Intent(this, PostActivity.class));
 		}
-		
+
+		if (v.getId() == R.id.buttonPost) {
+			startActivity(new Intent(this, PostActivity.class));
+		}
+
 	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		Toast.makeText(getApplicationContext(),
+				"Click ListItem Number " + position, Toast.LENGTH_LONG).show();
+
+	}
+
+	class TwitterGetHomeline extends
+			AsyncTask<Void, Void, ResponseList<twitter4j.Status>> {
+		ResponseList<twitter4j.Status> statuses;
+
+		@Override
+		protected ResponseList<twitter4j.Status> doInBackground(Void... params) {
+
+			twitter = ((TwittApplication) getApplication()).getTwitter();
+			try {
+				statuses = twitter.getHomeTimeline();
+				return statuses;
+
+			} catch (TwitterException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+
+		protected void onPostExecute(ResponseList<twitter4j.Status> result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+
+			if (result != null) {
+				listV = (ListView) findViewById(android.R.id.list);
+				listV.setAdapter(new TweetAdapter(TweetsActivity.this,
+						R.layout.tweet_row, statuses));
+
+			}
+		}
+	}
+
 }
